089fb61b732c0109b886cb1fd4286704c31f1bdb
PANKAJ SINGH KUSHWAHA <145460446+PANKAJ-SINGH-KUSHWAHA@users.noreply.github.com>
2025-10-15 09:37:59 +0530

    create the frontend structure through vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div class='font-bold text-red-500 text-3xl'>Hello How are you! Welcome to Library Management System</div>
+  )
+}
+
+export default App
