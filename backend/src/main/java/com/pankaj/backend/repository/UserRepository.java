238faf400d2b45579ccb52f1f9d7ab6880331354
PANKAJ SINGH KUSHWAHA <145460446+PANKAJ-SINGH-KUSHWAHA@users.noreply.github.com>
2025-10-19 23:00:48 +0530

    resolve the issue of UserRepository

--- backend/src/main/java/com/pankaj/backend/repository/UserRepository.java
+++ backend/src/main/java/com/pankaj/backend/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.pankaj.backend.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByVerificationCode(String verificationCode);
